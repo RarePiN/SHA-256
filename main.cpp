50699694ae5d9d5f37ae549e21b4205a0bb425c3
LuckyNow <88903945+RarePiN@users.noreply.github.com>
2025-04-27 05:26:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 class SHA256{
         private:
         int msgLength;  // In bit of binary
         int paddedLength;
-
+        string h[8];
         // HASH VALUES
-        string h0 = "6a09e667";         //6a09e667
-        string h1 = "bb67ae85";         //bb67ae85
-        string h2 = "3c6ef372";         //3c6ef372
-        string h3 = "a54ff53a";         //a54ff53a
-        string h4 = "510e527f";         //510e527f
-        string h5 = "9b05688c";         //9b05688c
-        string h6 = "1f83d9ab";         //1f83d9ab
-        string h7 = "5be0cd19";         //5be0cd19
+
+        void resetHashValue() {
+                h[0] = "6a09e667";
+                h[1] = "bb67ae85";
+                h[2] = "3c6ef372";
+                h[3] = "a54ff53a";
+                h[4] = "510e527f";
+                h[5] = "9b05688c";
+                h[6] = "1f83d9ab";
+                h[7] = "5be0cd19";
+
+        }
 
         string k[64] = {
                 "428a2f98","71374491","b5c0fbcf","e9b5dba5","3956c25b","59f111f1","923f82a4","ab1c5ed5",
@@ -33,21 +36,9 @@ class SHA256{
         }
 
         void getPadLen(string l) {
-                int c = l.size() / 512;
-                int r = l.size() % 512;
-                int temp = c * 512;
-                if (r > 0) temp += 512;
-                paddedLength = temp;
-        }
-
-        string hexToBin(string l) {        // hex to 32 bit-binary
-                if (l.size() >= 3) if (l[0] == '0' and l[1] == 'x') l = l.substr(2,l.size() - 2);
-                if (l.size() == 1 and l[0] == 0) return "00000000000000000000000000000000";
-
-                string output = "";
-                for (int i = 0; i < l.size(); i++) {
-                        char temp = l[size
-                }
+                int bits = msgLength;
+                paddedLength = ((bits + 65) / 512 + 1) * 512;
+                if (bits + 65 <= 512) paddedLength = 512;
         }
 
         string rightRotate(string l, int n) {
@@ -72,6 +63,26 @@ class SHA256{
                 }
 
                 return temp;
+        }
+
+        string NOT(string l) {
+                string output = "";
+                for (int i = 0; i < l.size(); i++) {
+                        if (l[i] == '0') output += '1';
+                        else output += '0';
+                }
+
+                return output;
+        }
+
+        string AND(string l1, string l2) {
+                string output = "";
+                for (int i = 0; i < l1.size(); i++) {
+                        if (l1[i] == l2[i]) output += '1';
+                        else output += '0';
+                }
+
+                return output;               
         }
 
         string sigma0(int i, string *w) {
@@ -121,20 +132,12 @@ class SHA256{
             
                 return result;
         }
-
-        string toBin(int num) { // Decimal to binary (8-bit)
-                if (num == 0) return "00000000";
-                string temp = "";
-                while (num > 0) {
-                        int remainder = num % 2;
-                        temp += ('1' + remainder - 1);
-                        num = num / 2;;
+        string toBin(int num) {
+                string output(8, '0');
+                for (int i = 7; i >= 0; i--) {
+                        output[i] = (num & 1) ? '1' : '0';
+                        num >>= 1;
                 }
-
-                while (temp.size() < 8) temp += '0';
-
-                string output = "        ";
-                for (int i = 0; i < 8; i++) output[7-i] = temp[i];
                 return output;
         }
 
@@ -154,11 +157,44 @@ class SHA256{
                 return output;
         }
 
-        string hexToBin(string l) {
-                int power = 0;
+        string hexToBin(string l) {     // 32-bit binary
+                if (l.size() == 0) return "00000000000000000000000000000000";
+                if (l.size() > 2) if (l[0] == '0' and l[0] == 'x') l = l.substr(2,l.size()-2);
+                
+                string output;
                 for (int i = 0; i < l.size() ; i++) {
-
+                        int temp;
+                        if (l[i] >= '0' and l[i] <= '9') {
+                                temp = l[i] - '0';
+                        } else if (l[i] >= 'a' and l[i] <= 'f') {
+                                temp = l[i] - 'a' + 10;
+                        }
+                        
+                        bool digit[4] = {false,false,false,false};
+                        if (temp >= 8) {
+                                temp -= 8;
+                                digit[0] = true;
+                        }
+                        if (temp >= 4) {
+                                temp -= 4;
+                                digit[1] = true;
+                        }
+                        if (temp >= 2) {
+                                temp -= 2;
+                                digit[2] = true;
+                        }
+                        if (temp >= 1) {
+                                temp -= 1;
+                                digit[3] = true;
+                        }
+                        
+                        for (int i = 0; i < 4; i++) {
+                                if (digit[i]) output += '1';
+                                else output += '0';
+                        }
                 }
+                
+                return output;
         }
 
         string textToBin(string l) {
@@ -172,16 +208,60 @@ class SHA256{
 
         string preProccess(string l) {
                 l += '1';
-                for (int i = msgLength; i < paddedLength - 65; i++) {
-                        l += '0';
-                }
-
+                // Calculate zeros needed: paddedLength - msgLength - 65
+                int zeros_needed = paddedLength - msgLength - 65;
+                l += string(zeros_needed, '0');
                 l += msgSize(msgLength);
-
                 return l;
         }
 
+        char chunkToChar(string l) {
+                int n = 0;
+                
+                while (l.size() < 4) {
+                        l = '0' + l;
+                }
+                
+                if (l[0] == '1') n += 8;
+                if (l[1] == '1') n += 4;
+                if (l[2] == '1') n += 2;
+                if (l[3] == '1') n += 1;
+                
+                if (n < 10) {
+                        return '0' + n;
+                } else {
+                        return 'a' + (n - 10);
+                }
+            }
+    
+        string binToHex(string l) {
+                string head = "-";
+                string chunk[(l.size() / 4)];
+                int count = 0;
+                if (l.size() % 4 != 0) {
+                        head = l.substr(0,l.size() % 4);
+                }
+                    
+                l = l.substr(l.size() % 4, l.size() - (l.size() % 4));
+                    
+                for (int i = 0; i < l.size(); i++) {
+                        chunk[count] += l[i];
+                        if ((i + 1) % 4 == 0) {
+                                count++;
+                        }
+                }
+                    
+                string output = "";
+                if (head != "-") output += chunkToChar(head);
+                for (int i = 0; i < (l.size() / 4); i++) {
+                        output += chunkToChar(chunk[i]);
+                }
+                    
+                return output;
+        }
+
         string computation(string l) {
+                resetHashValue();
                 string m[16];
                 for (int i = 0; i < 16; i++) {
                         m[i] = l.substr(i * 32,32);
@@ -199,24 +279,54 @@ class SHA256{
                         w[i] = addtion(addtion(addtion(w[i-16], sigma0(i,w)), w[i-7]), sigma1(i,w));
                 }
 
-                for (int i = 0; i < 64 ; i++) {
-                        cout << w[i] << " ";
-                        if ((i + 1) % 4 == 0) cout << endl;
+                string value[8];
+                for (int i = 0; i < 8; i++) value[i] = hexToBin(h[i]);
+
+                for (int i = 0; i < 64; i++) {
+                        string S1, S0, ch, maj, temp1, temp2;
+                        S1 = xOR(rightRotate(value[4],6), xOR(rightRotate(value[4],11),rightRotate(value[4],25)));
+                        S0 = xOR(rightRotate(value[0],2), xOR(rightRotate(value[0],13),rightRotate(value[0],22)));
+                        ch = xOR(AND(value[4],value[5]),AND(NOT(value[4]),value[6]));
+                        maj = xOR(AND(value[0],value[1]), xOR(AND(value[0],value[2]), AND(value[1],value[2])));
+                        temp1 = addtion(value[7],addtion(S1,addtion(ch,addtion(hexToBin(k[i]),w[i]))));
+                        temp2 = addtion(S0, maj);
+
+                        value[7] = value[6];
+                        value[6] = value[5];
+                        value[5] = value[4];
+                        value[4] = addtion(value[3],temp1);
+                        value[3] = value[2];
+                        value[2] = value[1];
+                        value[1] = value[0];
+                        value[0] = addtion(temp1,temp2);
+
                 }
-                return "";
+
+                for (int i = 0; i < 8; i++) {
+                        h[i] = addtion(hexToBin(h[i]),value[i]);
+                }
+
+                string output;
+                for (int i = 0; i < 8; i++) {
+                        output += h[i];
+                }
+
+                return binToHex(output);
         }
 
         public:
 
         void test(string l) {
                 getMsgLen(l);
-                getPadLen(l);
+                getPadLen(l);   
                 string temp = preProccess(textToBin((l)));
-                cout << computation(temp);
+                cout << endl << computation(temp) << endl;
         }
 };
 
 int main() {
         SHA256 test;
-        test.test("hello");
+        string l = "";
+        cin >> l;
+        test.test(l);
 }
